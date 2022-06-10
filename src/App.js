be60b3818ee07fc959a92a57ed4375c55ab10a8b
Wanglala <sue4100035045@gmail.com>
2022-06-10 19:08:42 +0800

    feat: add default setting and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import { ResetStyle, GlobalStyle } from './components/globalStyle';
+import { PageHeader } from './components/header/Header';
+import { theme } from './components/variables';
+
 const App = () => {
-  return <div className="App">reaact</div>;
+  return (
+    <div className="App">
+      <ResetStyle />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <PageHeader />
+        reaact
+      </ThemeProvider>
+    </div>
+  );
 };
 
 export default App;
